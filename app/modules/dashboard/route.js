db4d4a690a6c80fabcaa6cec4be45958118ab004
Navtej Pal Singh <navtej9173@gmail.com>
2017-06-04 23:21:15 +0530

    Initial Commit

---
+++ app/modules/dashboard/route.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name test1App
+ * @description
+ * # test1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('test1App').config(function($routeProvider){
+    $routeProvider
+    .when('/',{
+        redirectTo: '/dashboard'
+    })
+    .when('/dashboard',{
+        templateUrl: 'modules/dashboard/dashboard.html'
+    })
+    .when('/home',{
+        templateUrl: 'views/home.html'
+    })
+    .when('/about',{
+        templateUrl: 'views/about.html'
+    })
+    .otherwise({
+        templateUrl: '404.html'
+        //redirectTo: '/'
+    });
+  });
---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name test1App
+ * @description
+ * # test1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('test1App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+  ]);
